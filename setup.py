2bc42926d45e940efaa18b0c0b0c903ec73841cc
2xkgames <kuzeykural040510@gmail.com>
2024-09-02 17:14:21 +0300

    Initial commit of pycolourslib

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycolourslib',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     description='a python module with variabled colours for more simplicity and less time-wasting in your projects',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    author='KZed-K',
+    author='QZ-K',
     author_email='kuzeykural040510@gmail.com',
     url='https://github.com/KZed-K/pycolourslib', 
     classifiers=[
